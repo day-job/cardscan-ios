432be86305e01fca9055ecad85c92ed34c1fafdf
Zain <dinxaen@gmail.com>
2020-03-29 21:30:21 -0700

    run findfour in ocr.swift

---
+++ CardScan/Classes/OcrDD.swift
@@ -0,0 +1,26 @@
+//
+//  OcrDD.swift
+//  CardScan
+//
+//  Created by xaen on 3/29/20.
+//
+
+import Foundation
+public class OcrDD{
+    public init(){}
+
+    static func configure(){
+        if #available(iOS 11.2, *){
+            let ssdOcr = SSDOcrDetect()
+            ssdOcr.warmUp()
+        }
+    }
+
+    @available(iOS 11.2, *)
+    public func perform(croppedCardImage: CGImage) -> String?{
+        var ssdOcr = SSDOcrDetect()
+        var number = ssdOcr.predict(image: UIImage(cgImage: croppedCardImage))
+        return number
+    }
+
+}
